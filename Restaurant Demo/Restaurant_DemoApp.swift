4381043d6d2931483aeb1c31657aa5f1adf8e5fa
Jeremiah Wiseman <jeremiahw12310@gmail.com>
2025-06-18 01:02:25 -0500

    Initial Commit

---
+++ Restaurant Demo/Restaurant_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant_DemoApp.swift
+//  Restaurant Demo
+//
+//  Created by Jeremiah Wiseman on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Restaurant_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Restaurant DemoTests/Restaurant_DemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant_DemoTests.swift
+//  Restaurant DemoTests
+//
+//  Created by Jeremiah Wiseman on 6/18/25.
+//
+
+import Testing
+@testable import Restaurant_Demo
+
+struct Restaurant_DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
